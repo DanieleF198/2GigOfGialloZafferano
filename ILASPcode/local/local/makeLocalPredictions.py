d3279d3555380c429b1eafb0d1cc7d271b4ceb60
Joker italiano98 <DanieleF198>
2022-12-26 17:34:29 +0100

    Revert "little changes"
    This reverts commit 42069d25a41133a486b1ef0aa9878c9b3e3686c1.

--- ILASPcode/local/local/makeLocalPredictions.py
+++ ILASPcode/local/local/makeLocalPredictions.py
@@ -71,95 +71,11 @@ def find_samples(first_food, second_food, all_food, pca_food, number_of_sample, 
     print(to_print)
     print("--------------------distances from original couple--------------------")
     print(to_print2)
-    plt.scatter(pca_food[:, 0], pca_food[:, 1])
-    plt.scatter(to_return2[:, 0], to_return2[:, 1])
-    plt.scatter(pca_food[index_first_food, 0], pca_food[index_first_food, 1])
-    plt.scatter(pca_food[index_second_food, 0], pca_food[index_second_food, 1])
-    plt.xlabel('PC1 - {:.1f}%'.format(per_var[0]))
-    plt.ylabel('PC2 - {:.1f}%'.format(per_var[1]))
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.show()
-    plt.clf()
-    return to_return, to_return2
-
-
-def find_samples_2(first_food, second_food, all_food, pca_food, number_of_sample, index_first_food, index_second_food):
-    counter = 0
-    distances_metric = np.zeros(shape=(4950, 3), dtype='float32')   # see sushi_paper
-    for i1 in range(0, 100):
-        for i2 in range(i1 + 1, 100):
-            temp1 = 0
-            temp2 = 0
-            for index_element in range(0, 2):
-                for f in range(0, len(all_food[0])):
-                    if index_element == 0:
-                        if f == 0:
-                            temp1 += pow((0 if all_food[i1, f] == first_food[f] else 3), 2)
-                        temp1 += pow((abs(all_food[i1, f] - first_food[f])), 2)
-                    else:
-                        if f == 0:
-                            temp2 += pow((0 if all_food[i2, f] == second_food[f] else 3), 2)
-                        temp2 += pow((abs(all_food[i2, f] - second_food[f])), 2)
-                if index_element == 0:
-                    temp1 = math.sqrt(temp1)
-                else:
-                    temp2 = math.sqrt(temp2)
-            distances_metric[counter, 0] = i1
-            distances_metric[counter, 1] = i2
-            distances_metric[counter, 2] = round(temp1 + temp2)
-            counter += 1
-
-    ind = np.argsort(distances_metric[:, -1])
-    distances_metric_sorted = distances_metric[ind]
-    to_return = np.zeros((number_of_sample, len(all_food[0])*2))
-    to_return2 = np.zeros((number_of_sample, 34))
-    counter = 0
-    index = 0
-    to_print = ""
-    to_print2 = ""
-    probabilistic_discounter = 10
-    while True:
-        if counter == number_of_sample:
-            break
-        if distances_metric_sorted[index, 2] == 0:  # we only want sampled data, not original
-            index += 1
-            continue
-        if (int(distances_metric_sorted[index, 0]) == index_first_food) or (int(distances_metric_sorted[index, 1]) == index_second_food):
-            index += 1
-            continue
-        temp_concat = np.concatenate((all_food[int(distances_metric_sorted[index, 0])], all_food[int(distances_metric_sorted[index, 1])]))
-        temp_concat_2 = np.concatenate((pca_food[int(distances_metric_sorted[index, 0])], pca_food[int(distances_metric_sorted[index, 1])]))
-        to_print = to_print + str(int(distances_metric_sorted[index, 0])) + "," + str(int(distances_metric_sorted[index, 1])) + ";"
-        to_print2 = to_print2 + str(distances_metric_sorted[index, 2]) + ";"
-        probability_to_pick = (1-(distances_metric_sorted[index, 2]/distances_metric_sorted[4949, 2]))*100
-        choice_to_pick = random.choices([0, 1], weights=(100+probabilistic_discounter-probability_to_pick, probability_to_pick-probabilistic_discounter), k=1)
-        if choice_to_pick[0] == 1:
-            to_return[counter] = temp_concat
-            to_return2[counter] = temp_concat_2
-            index += 1
-            counter += 1
-            probabilistic_discounter += 5
-        else:
-            print("WOW: " + str(100+probabilistic_discounter-probability_to_pick))
-            probabilistic_discounter = 10
-            counter += 1
-    print(to_print)
-    print("--------------------distances from original couple--------------------")
-    print(to_print2)
-    plt.scatter(pca_food[:, 0], pca_food[:, 1])
-    plt.scatter(to_return2[:, 0], to_return2[:, 1])
-    plt.scatter(pca_food[index_first_food, 0], pca_food[index_first_food, 1])
-    plt.scatter(pca_food[index_second_food, 0], pca_food[index_second_food, 1])
-    plt.xlabel('PC1 - {:.1f}%'.format(per_var[0]))
-    plt.ylabel('PC2 - {:.1f}%'.format(per_var[1]))
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.show()
-    plt.clf()
     return to_return, to_return2
 
 
 def prediction(user_id, samples_to_predict):
-    model = keras.models.load_model('../../../NN_data/models/User' + str(user_id) + '/folder_version')
+    model = keras.models.load_model('../../NN_data/models/User' + str(user_id) + '/folder_version')
     predictions = model(samples_to_predict)
     to_print = ""
     for index, predict in enumerate(predictions):
@@ -182,10 +98,10 @@ def prediction(user_id, samples_to_predict):
 
 
 def single_prediction(user_id, first_food, second_food):
-    model = keras.models.load_model('../../../NN_data/models/User' + str(user_id) + '/folder_version')
+    model = keras.models.load_model('../../NN_data/models/User' + str(user_id) + '/folder_version')
     to_predict = np.concatenate((first_food, second_food))
     to_predict_reshaped = to_predict.reshape(1, -1)
-    predictions = model.predict(to_predict_reshaped, verbose=0)
+    predictions = model.predict(to_predict_reshaped)
     to_print = ""
     for index, predict in enumerate(predictions):
         if index >= 105:
@@ -209,15 +125,15 @@ def single_prediction(user_id, first_food, second_food):
 
 
 
-data_dir = "../../../dataset_100/separated_text_data/"
+data_dir = "../../dataset_100/separated_text_data/"
 
 fScalar = os.path.join(data_dir, 'scalars.txt')
 fCategories = os.path.join(data_dir, 'categories.txt')
 fIngredients = os.path.join(data_dir, 'ingredients.txt')
 fPreparation = os.path.join(data_dir, 'preparations.txt')
 fNames = os.path.join(data_dir, 'names.txt')
-fCouple = os.path.join('../../../Ordinamenti/dataset_coppie.txt')
-fLabels = os.path.join('../../../Ordinamenti/output-file.txt')
+fCouple = os.path.join('../../Ordinamenti/dataset_coppie.txt')
+fLabels = os.path.join('../../Ordinamenti/output-file.txt')
 
 fS = open(fScalar)
 dataS = fS.read()
@@ -330,10 +246,6 @@ pca.fit(final_data)
 # apply pca on data in pandas_dataframe
 pca_data = pca.transform(final_data)
 
-per_var = np.round(pca.explained_variance_ratio_*101, decimals=1)
-labels = ['PC' + str(x) for x in range(1, len(per_var)+1)]
-pca_df = pd.DataFrame(pca_data, columns=labels)
-
 # outliers: 15, 39, 45, 46, 53, 54, 56
 
 while True:
@@ -365,7 +277,7 @@ while True:
         print("user_id must be 0 < food_id < 47")
         continue
 
-    X, X_PCA = find_samples_2(first_food=all_data_original[int(first_food_id_to_pass)], second_food=all_data_original[int(second_food_id_to_pass)], all_food=all_data_original, pca_food=pca_data, number_of_sample=45, index_first_food=int(first_food_id_to_pass), index_second_food=int(second_food_id_to_pass))
+    X, X_PCA = find_samples(first_food=all_data_original[int(first_food_id_to_pass)], second_food=all_data_original[int(second_food_id_to_pass)], all_food=all_data_original, pca_food=pca_data, number_of_sample=45, index_first_food=int(first_food_id_to_pass), index_second_food=int(second_food_id_to_pass))
     # plt.scatter(pca_data[:, 0], pca_data[:, 1])
     # plt.scatter(X[:, 0], X[:, 1])
     # plt.scatter(pca_data[int(first_food_id_to_pass), 0], pca_data[int(first_food_id_to_pass), 1])
@@ -397,14 +309,14 @@ while True:
     if want == "Y" or want == "y":
         continue
 
-# recipes = random.sample(range(0, 100), 2)   # first random choice (TRAIN) 9, 47 (considering plot with PC1 and PC2 and cluster with k=3, 9 is near <0, 2> - <yellow cluster, on bound>; while 47 is near <2.2, -1.9> - <blue cluster, near centroid>) which seems a good choice for experiment (not too far but not too close)
+# recipes = random.sample(range(0, 100), 2) # first random choice (TRAIN) 9, 47 (considering plot with PC1 and PC2 and cluster with k=3, 9 is near <0, 2> - <yellow cluster, on bound>; while 47 is near <2.2, -1.9> - <blue cluster, near centroid>) which seems a good choice for experiment (not too far but not too close)
 # first_food_id_to_pass = recipes[0]
 # second_food_id_to_pass = recipes[1]
 
 # first_food_id_to_pass = 9
 # second_food_id_to_pass = 47
 
-# To test I've picked by hand 28 and 87, because they are near to symmetry with 9 and 47
+# for test i picked by hand 28 and 87, because they are near to symmetry with 9 and 47
 first_food_id_to_pass = 28
 second_food_id_to_pass = 87
 print("--------------------couple queried--------------------")
@@ -413,7 +325,7 @@ print("--------------------prediction of i-th user's model on queried couple----
 for i in range(0, 48):
     single_prediction(user_id=i, first_food=pca_data[int(first_food_id_to_pass)], second_food=pca_data[int(second_food_id_to_pass)])
 print("--------------------generation samples for train--------------------")
-X, X_PCA = find_samples_2(first_food=all_data_original[int(first_food_id_to_pass)], second_food=all_data_original[int(second_food_id_to_pass)], all_food=all_data_original, pca_food=pca_data, number_of_sample=105, index_first_food=int(first_food_id_to_pass), index_second_food=int(second_food_id_to_pass))
+X, X_PCA = find_samples(first_food=all_data_original[int(first_food_id_to_pass)], second_food=all_data_original[int(second_food_id_to_pass)], all_food=all_data_original, pca_food=pca_data, number_of_sample=105, index_first_food=int(first_food_id_to_pass), index_second_food=int(second_food_id_to_pass))
 print("--------------------generation label for train--------------------")
 for i in range(0, 48):
     prediction(user_id=i, samples_to_predict=X_PCA)
